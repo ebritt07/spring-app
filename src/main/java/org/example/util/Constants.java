5c51907c1fbe3c3b95b42cd51416dfc4c02342c8
ebritt07 <44182357+ebritt07@users.noreply.github.com>
2025-05-25 13:04:07 -0400

    add badges branch

--- src/main/java/org/example/util/Constants.java
+++
@@ -1,5 +0,0 @@
-package org.example.util;
-
-public class Constants {
-    public static String SAME_ORIGIN_DESTINATION = "Origin and Destination cannot be the same.";
-}
--- src/test/java/org/example/TestApplication.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class TestApplication extends MainApplication {
-    public static void main(final String... args) {
-//        System.setProperty("spring.config.name", "application-test");
-        MainApplication.main(args);
-    }
-}
